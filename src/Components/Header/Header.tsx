4d4de2e2c87a00adb6d8c5ea712e394c718bda41
Stas9364 <stas-lisovskii@mail.ru>
2022-07-08 19:13:00 +0300

    add appReducer test

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,21 +7,27 @@ type HeaderPropsType = InitStateType & {
     logout: () => void
 }
 
-export const Header: React.FC<HeaderPropsType> =  ({
-                                                       login,
-                                                       isAuth,
-                                                       logout
-}) => {
+export const Header: React.FC<HeaderPropsType> = ({
+                                                      login,
+                                                      isAuth,
+                                                      logout
+                                                  }) => {
     return (
         <header className={style.header}>
             <img src={img} alt='ape'></img>
             <div className={style.loginTitle}>
 
-
                 {isAuth
-                    ? <div>{login} <button onClick={logout} className={style.myButton}>LogOut</button></div>
-                    : <span className={style.login}>Welcome to our network!</span>
+                    ? <div>{login}
+                        <button onClick={logout} className={style.myButton}>
+                            LogOut
+                        </button>
+                    </div>
+                    : <span className={style.login}>
+                        Welcome to our network!
+                    </span>
                 }
+
             </div>
         </header>
     );
